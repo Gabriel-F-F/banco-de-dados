cb71ec7086d026346553aa5bef06b1643faf510c
Gabriel-F-F <gfelixfaustina@gmail.com>
2024-08-14 17:09:30 -0300

    BD_berçário_físico

--- Projetos/Postgre/bercario_fisico.sql
+++ Projetos/Postgre/bercario_fisico.sql
@@ -1,10 +1,10 @@
 ﻿-- DROP MODO DEV
+DROP TABLE IF EXISTS bebe;
+DROP SEQUENCE IF EXISTS bebe_id_seq;
 DROP TABLE IF EXISTS mae;
 DROP SEQUENCE IF EXISTS mae_id_seq;
 DROP TABLE IF EXISTS medico;
 DROP SEQUENCE IF EXISTS medico_id_seq;
-DROP TABLE IF EXISTS bebe;
-DROP SEQUENCE IF EXISTS bebe_id_seq;
 
 -- TABELA MÃE
 CREATE SEQUENCE IF NOT EXISTS mae_id_seq;
@@ -55,5 +55,13 @@ CREATE TABLE IF NOT EXISTS bebe (
 
 INSERT INTO medico (crm, nome, telefone_celular, especialidade) VALUES (12345, 'João da Silva', '(48) 11111-1111', 'Pediatra');
 INSERT INTO medico (crm, nome, telefone_celular, especialidade) VALUES (6789, 'Maria da Silva', '(48) 22222-2222', 'Oftamologista');
-INSERT INTO medico (crm, nome, telefone_celular, especialidade) VALUES (13579, 'Carlos Adrino', '(48) 33333-3333', 'Cirurgião');
 INSERT INTO medico (crm, nome, telefone_celular, especialidade) VALUES (51347, 'Clavisson José', '(48) 44444-4444', 'Anetesista');
+
+INSERT INTO mae (nome, endereco, telefone, dataNascimento) VALUES ('Maria Joaquina', 'Avenida Pedroso', '(48) 11111-1111', '1989-09-28');
+INSERT INTO mae (nome, endereco, telefone, dataNascimento) VALUES ('Valmira Duarte', 'Rua das Gaivotas', '(48) 22222-2222', '1992-04-12');
+INSERT INTO mae (nome, endereco, telefone, dataNascimento) VALUES ('Ana Paula', 'BR-101', '(48) 33333-33333', '1995-02-10');
+
+INSERT INTO bebe (id_mae, id_medico, nome, dataNascimento, pesoNascimento) VALUES (1, 1, 'Pedrinho', '2023-09-23', 1.65);
+INSERT INTO bebe (id_mae, id_medico, nome, dataNascimento, pesoNascimento) VALUES (2, 1, 'Zézinho', '2022-03-01', 1.78);
+INSERT INTO bebe (id_mae, id_medico, nome, dataNascimento, pesoNascimento) VALUES (3, 3, 'Franjinha', '2020-05-12', 1.54);
+INSERT INTO bebe (id_mae, id_medico, nome, dataNascimento, pesoNascimento) VALUES (3, 2, 'Franjinha', '2020-05-12', 1.54);
