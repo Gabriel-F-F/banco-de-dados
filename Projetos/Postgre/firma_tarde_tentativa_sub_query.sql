ef0d727baa216fb357db00e17a45b210fdf82d82
Gabriel-F-F <gfelixfaustina@gmail.com>
2024-08-22 17:08:00 -0300

    Commit Aula 22/08/2024

--- Projetos/Postgre/firma_tarde_tentativa_sub_query.sql
+++ Projetos/Postgre/firma_tarde_tentativa_sub_query.sql
@@ -1,12 +1,10 @@
 ﻿SELECT
-	cliente_pf.nome AS nome,
-	cliente_pj.razao_social AS nome
-FROM cliente
-LEFT JOIN cliente_pf ON (cliente_pf.id = cliente.id)
-LEFT JOIN cliente_pj ON (cliente_pj.id = cliente.id)
-INNER JOIN pedido ON (pedido.id_cliente = cliente.id)
-WHERE cliente.id IN (
-SELECT
-	pedido.id
-FROM pedido
-WHERE pedido.id_cliente = cliente.id);
+	cliente_pf.nome AS nome_fisico,
+	SUM(item_pedido.quantidade) AS quantidade_pedidos
+FROM cliente_pf
+INNER JOIN pedido ON (pedido.id_cliente = cliente_pf.id)
+INNER JOIN item_pedido ON (item_pedido.id_pedido = pedido.id)
+WHERE cliente_pf.id IN (SELECT pedido.id_cliente
+	     FROM pedido
+	     WHERE id_cliente = cliente_pf.id)
+GROUP BY nome_fisico
