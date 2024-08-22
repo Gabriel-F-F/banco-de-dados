a83692c44991d59d205d9544c9db6982c6b5581d
Gabriel-F-F <gfelixfaustina@gmail.com>
2024-08-22 16:24:05 -0300

    Commit Subqueries

---
+++ Projetos/Postgre/firma_tarde_tentativa_sub_query.sql
@@ -0,0 +1,12 @@
+﻿SELECT
+	cliente_pf.nome AS nome,
+	cliente_pj.razao_social AS nome
+FROM cliente
+LEFT JOIN cliente_pf ON (cliente_pf.id = cliente.id)
+LEFT JOIN cliente_pj ON (cliente_pj.id = cliente.id)
+INNER JOIN pedido ON (pedido.id_cliente = cliente.id)
+WHERE cliente.id IN (
+SELECT
+	pedido.id
+FROM pedido
+WHERE pedido.id_cliente = cliente.id);
